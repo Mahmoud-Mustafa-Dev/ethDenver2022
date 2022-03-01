05de65aa3d09a936ae63b8da27acf7404d5c83e1
Mahmoud <mahmoudmustafadev@outlook.com>
2022-03-01 14:30:05 +0300

    add twitch time tracker and save sign in status

---
+++ LeapExtension/js/viewer-login.js
@@ -0,0 +1,36 @@
+/*document.getElementById('twitch-sign-in').addEventListener('click', function () {
+    chrome.runtime.sendMessage({ message: 'login-twitch' }, function (response) {
+        if (response.message === 'success') {
+            console.log("we signed in using twitch");
+        } 
+    });
+});
+
+document.getElementById('ytb-sign-in').addEventListener('click', function () {
+    chrome.runtime.sendMessage({ message: 'login-ytb' }, function (response) {
+        if (response.message === 'success') {
+            console.log("we signed in using ytb");
+        } 
+    });
+});
+*/
+
+//TODO login viewers via ytb or twitch
+
+document.getElementById('sync-wallet-btn').addEventListener('click', function () {
+   // sync_wallet();
+    console.log(localStorage["wallet-address"]);
+});
+
+function sync_wallet() {
+    //Pull text from user inputbox
+    var data = new String(document.getElementById("wallet-input").value);
+    console.log("length is: " + data.length);
+    if(data.length >= 26 && data.length <= 35) {
+    console.log("this wallet address is valid");
+    //Save it to the localStorage variable which will always remember what you store in it
+    localStorage["wallet-address"] = data; 
+    } else {
+    console.log("this wallet address is not valid");
+    }
+}
